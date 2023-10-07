51c7e89314c5acad18211b6cfc49c63dd43b0e2e
Raine Pretorius <raine.pretorius1@gmail.com>
2023-10-07 20:13:12 +0200

    Olarm API package

---
+++ src/olarm_api_rainepretorius/const.py
@@ -0,0 +1,13 @@
+"""Module that stores all the constants for the integration."""
+import logging
+VERSION = "1.0.0"
+
+GITHUB_TOKEN = "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"
+
+LOGGER = logging.getLogger(__package__)
+
+CONF_SCAN_INTERVAL = 5
+
+class ServiceCall:
+    def __init__(self):
+        pass
